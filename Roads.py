91a778bd35df3627d5ee30c9af11c86ed0e24110
diffy02 <dominic.rafa03@gmail.com>
2026-06-07 20:36:56 +0700

    nan
    don't mind with this, I was just uh removing some texts while I was at it ;)

--- Roads.py
+++ Roads.py
@@ -8,12 +8,11 @@ class Road(turtle.Turtle):
         super().__init__()
         self.hideturtle()
         self.color((164, 164, 164))
-        self.penup()  # Keep the pen up while initializing and moving
-        # Moving 'pick' inside __init__ ensures each road can have a UNIQUE width!
+        self.penup()
         self.pick = random.choice(pixels)
 
     def draw(self):
-        self.pendown()  # Put the pen down only to draw the road shape
+        self.pendown()
         self.fillcolor((164, 164, 164))
         self.begin_fill()
         for i in range(2):
